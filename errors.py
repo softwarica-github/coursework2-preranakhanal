11082f191d00d90a640623d03fa118763ad72afd
prerena <preranakhanal42@gmail.com>
2024-02-07 13:28:31 +0545

    completed errors

--- errors.py
+++ errors.py
@@ -11,4 +11,19 @@ def error(title,message):
     tfont_tup = ("Comic Sans MS", 15)
 
 
+    #function named no which when called destroys Toplevel
+    def no():
+        WIN_top.destroy()
 
+    #Message to be Displayed at Toplevel
+    message = Label(WIN_top, bg='#E0D9EF', text=message, font=tfont_tup, justify="center",
+                        foreground="#000000")
+    message.pack()
+
+
+    #No Button which when pressed calls no function
+    no_button = Button(WIN_top, bg='#FFFFFF', text=" Ok ", background='Red', foreground="Black", font=("Comic Sans MS", 12), command=no)
+    no_button.place(x=108, y=80)
+
+    #Places all GUI of Toplevel into it
+    WIN_top.mainloop()
